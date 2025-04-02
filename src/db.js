5447d889ab1156bcce56e33fb1a4dc868c6aca2e
Joshua Corrales <157666265+corralesj@users.noreply.github.com>
2025-04-01 22:04:02 -0600

    refactor database configuration to use environment variables

--- src/db.js
+++ src/db.js
@@ -1,11 +1,14 @@
 import { createPool } from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pool = createPool({
-  host: "localhost",   // Database host 
-  user: 'root',        // Database user
-  password: 'charlie',        // Database password
-  port: 3306,         // Database port
-  database: 'reque'   // Database name
+  host: process.env.DB_HOST,   // Database host 
+  user: process.env.DB_USER,        // Database user
+  password: process.env.DB_PASSWORD,        // Database password
+  port: process.env.DB_PORT,         // Database port
+  database: process.env.DB_NAME   // Database name
 
   })
 
