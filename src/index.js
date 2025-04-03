6934e2f4d53833ad134a14f0fb5e693b11a02a58
Carol Araya <carayacn@gmail.com>
2025-04-03 10:35:10 -0600

    puerto dinamico

--- src/index.js
+++ src/index.js
@@ -279,6 +279,8 @@ app.use((req, res) => {
   res.status(404).send('No se encontro la pagina')
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
